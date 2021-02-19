d6ba60c17fcd0f80997601874fd7e4b48d345bbf
Athailah <atha@arviga.co.id>
2021-02-19 11:50:25 +0700

    Menambahkan akses ke static folder untuk mengakses css, images, dan js secara terpusat
    - Athailah, S.Kom.

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Tambahan untuk static folder.
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]

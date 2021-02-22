be18b8b7794235f1acd8ce68e69bff787884c2fb
Athailah <atha@arviga.co.id>
2021-02-22 15:14:41 +0700

    menambahkan dinamic url untuk customer dan navbar

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.product),
-    path('customers/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.product, name="product"),
+    path('customers/<str:pk_test>/', views.customer, name="customer"),
+
 
 ]

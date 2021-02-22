e40f716151dbaadd8bdcf492cd4f3b1bff16ef45
Athailah <atha@arviga.co.id>
2021-02-22 18:12:33 +0700

    menambahkan inline formset ke form customer order

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import OrderForm
@@ -28,16 +29,17 @@ def customer(request, pk_test):
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request, 'accounts/customers.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer) 
     if request.method == 'POST':
-        #print('Printing POST:', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
